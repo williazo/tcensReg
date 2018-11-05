0d3990bfac752b0426698ab01a6816a6a8a01ef8
Justin Williams <williazo@ucla.edu>
2018-11-05 14:30:43 -0800

    I am adding in a new line of functions to calculate parameters for the case where we have separate independent truncated normals with different variances.

---
+++ R/tcensReg_llike_sepvar.R
@@ -0,0 +1,4 @@
+#' Log Likelihood for Model for J independent Truncated Normal Variables with Same Mean Structure but Different Variance
+#'
+#' This function is part of the supporting function space to calculate the value of the log-likelihood at the nth iteration of theta.
+#' For this parametrization we assume that there exists (p-1) linear parameters for the mean and J separate variance parameters
