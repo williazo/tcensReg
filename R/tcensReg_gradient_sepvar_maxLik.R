ec79f74f1d3303b8d5ce966abe2e7641fbef0ea5
Justin Williams <williazo@ucla.edu>
2019-10-31 09:07:11 -0700

    adding in specific functions for finding maxLik result for the heteroskedastic variance problem

---
+++ R/tcensReg_gradient_sepvar_maxLik.R
@@ -0,0 +1,130 @@
+#' Gradient Vector for Truncated Normal Distribution with Censoring with Linear Equation Mean for J Independent Truncated Normals with Seprate Variance
+#'
+#' @param theta Numeric vector numeric vector containing estimates of beta and log sigma
+#' @param left_trunc Numeric scalar indicating the truncation value
+#' @param v Numeric scalar indicating the censoring value
+#' @param y Numeric vector with the observed truncated and censored outcomes
+#' @param X Numeric design matrix
+#' @param group Character vector identifying the group membership for the independent truncated normal variables. This defines the \code{J} groups.
+#'
+#' @importFrom stats dnorm pnorm
+#'
+#' @return Vector of gradient values with p-1 beta parameters and log sigma for the nth iterate
+
+tcensReg_gradient_sepvar_maxLik <- function(theta, y, X, group, left_trunc = -Inf, v = NULL){
+
+    nabla <- vector(length = length(theta)) #creating an empty vector to store the gradient
+
+    #assume that there are a total of p parameters
+    p <- length(theta)
+    num_groups <- length(unique(group)) #quantifying the J groups
+    log_sigmas <- theta[(p-num_groups+1):p] #these are all of the separate standard deviation parameters
+    lin_pred <- theta[1:(p-num_groups)] #these are the parameters for the linear predictors
+
+    if(is.null(v) == FALSE){ #this is the censored only version
+
+        #calculating the gradient for each beta parameter
+        for(j in 1:length(lin_pred)){
+            d1 <- NULL; d2 <- NULL; d3 <- NULL; nabla_jk <- vector(length = num_groups)
+            for(k in 1:num_groups){
+                y_k <- y[group == unique(group)[k]]
+                X_k <- as.matrix(X[group == unique(group)[k],], nrow = length(group == unique(group)[k]), ncol = ncol(X)) #I want to ensure this is still a matrix in order to use the matrix multiplicaiton later
+                X_kj <- as.matrix(X[group == unique(group)[k], j], nrow = length(group == unique(group)[k]), ncol = ncol(X)) #I want to ensure this is still a matrix in order to use the matrix multiplicaiton later
+                n_k <- length(y_k) #number of observations in jth group
+
+                uncens <- which(y_k>v) #identifying which values are uncensored
+                cens <- which(y_k == v) #idenfitying which values are censored
+
+                n_0k <- length(cens) #number of censored observations
+                n_1k <- length(uncens) #number of uncensored observations
+                a_stand <- (left_trunc-X_k%*%lin_pred)/exp(log_sigmas[k]) #standardized value with respect to truncated value
+                v_stand <- (v-X_k%*%lin_pred)/exp(log_sigmas[k]) #standardized value with respect to censored value
+                y_stand <- (y_k-X_k%*%lin_pred)/exp(log_sigmas[k])
+
+
+                d1 <- sum((-X_kj*exp(-log_sigmas[k])*dnorm(a_stand))/pnorm(-a_stand))
+                d2 <- sum((X_kj[cens]*exp(-log_sigmas[k])*(dnorm(v_stand[cens])-dnorm(a_stand[cens])))/(pnorm(v_stand[cens])-pnorm(a_stand[cens])))
+                d3 <- sum(X_kj[uncens]*exp(-log_sigmas[k])*(y_stand[uncens]))
+                nabla_jk[k] <- d1 - d2 + d3
+            }
+            nabla[j] <- sum(nabla_jk)
+        }
+        k <- 1 #indicator for looping over the groups
+        for(j in (p-num_groups+1):p){ #looping over the sigma parameters
+            y_k <- y[group == unique(group)[k]]
+            X_k <- as.matrix(X[group == unique(group)[k],], nrow = length(group == unique(group)[k]), ncol = ncol(X)) #I want to ensure this is still a matrix in order to use the matrix multiplicaiton later
+            n_k <- length(y_k) #number of observations in jth group
+
+            uncens <- which(y_k>v) #identifying which values are uncensored
+            cens <- which(y_k == v) #idenfitying which values are censored
+
+            n_0k <- length(cens) #number of censored observations
+            n_1k <- length(uncens) #number of uncensored observations
+            a_stand <- (left_trunc-X_k%*%lin_pred)/exp(log_sigmas[k]) #standardized value with respect to truncated value
+            v_stand <- (v-X_k%*%lin_pred)/exp(log_sigmas[k]) #standardized value with respect to censored value
+            y_stand <- (y_k-X_k%*%lin_pred)/exp(log_sigmas[k])
+
+            #here we need a special case when there is no truncation (i.e. a = -Inf)
+            if(left_trunc == -Inf){
+                d1 <- 0
+                d2 <- sum((v_stand[cens]*dnorm(v_stand[cens]))/(pnorm(v_stand[cens])))
+                d3 <- n_1k
+                d4 <- sum(y_stand[uncens]^2)
+                nabla[j] <- d1 - d2 - d3 + d4
+            }else{
+                d1 <- sum((-a_stand*dnorm(a_stand))/pnorm(-a_stand))
+                d2 <- sum((v_stand[cens]*dnorm(v_stand[cens])-a_stand[cens]*dnorm(a_stand[cens]))/(pnorm(v_stand[cens])-pnorm(a_stand[cens])))
+                d3 <- n_1k
+                d4 <- sum(y_stand[uncens]^2)
+                nabla[j] <- d1 - d2 - d3 + d4
+            }
+            k <- k + 1
+        }
+    }else if(is.null(v)==TRUE){
+        #calculating the gradient for each beta parameter
+        for(j in 1:length(lin_pred)){
+            d1 <- NULL; d2 <- NULL; d3 <- NULL; nabla_jk <- vector(length = num_groups)
+            for(k in 1:num_groups){
+                y_k <- y[group == unique(group)[k]]
+                X_k <- as.matrix(X[group == unique(group)[k],], nrow = length(group == unique(group)[k]), ncol = ncol(X)) #I want to ensure this is still a matrix in order to use the matrix multiplicaiton later
+                X_kj <- as.matrix(X[group == unique(group)[k], j], nrow = length(group == unique(group)[k]), ncol = ncol(X)) #I want to ensure this is still a matrix in order to use the matrix multiplicaiton later
+                n_k <- length(y_k) #number of observations in jth group
+
+
+                a_stand <- (left_trunc-X_k%*%lin_pred)/exp(log_sigmas[k]) #standardized value with respect to truncated value
+                y_stand <- (y_k-X_k%*%lin_pred)/exp(log_sigmas[k])
+
+
+                d1 <- sum((-X_kj*exp(-log_sigmas[k])*dnorm(a_stand))/pnorm(-a_stand))
+                d3 <- sum(X_kj*exp(-log_sigmas[k])*(y_stand))
+                nabla_jk[k] <- d1 + d3
+            }
+            nabla[j] <- sum(nabla_jk)
+        }
+
+        k <- 1 #indicator for looping over the groups
+        for(j in (p-num_groups+1):p){ #looping over the sigma parameters
+            y_k <- y[group == unique(group)[k]]
+            X_k <- as.matrix(X[group == unique(group)[k],], nrow = length(group == unique(group)[k]), ncol = ncol(X)) #I want to ensure this is still a matrix in order to use the matrix multiplicaiton later
+            n_k <- length(y_k) #number of observations in jth group
+
+            a_stand <- (left_trunc-X_k%*%lin_pred)/exp(log_sigmas[k]) #standardized value with respect to truncated value
+            y_stand <- (y_k-X_k%*%lin_pred)/exp(log_sigmas[k])
+
+            #here we need a special case when there is no truncation (i.e. a = -Inf)
+            if(left_trunc == -Inf){
+                d1 <- 0
+                d3 <- n_1k
+                d4 <- sum(y_stand^2)
+                nabla[j] <- d1 - d3 + d4
+            }else{
+                d1 <- sum((-a_stand*dnorm(a_stand))/pnorm(-a_stand))
+                d3 <- n_1k
+                d4 <- sum(y_stand^2)
+                nabla[j] <- d1 - d3 + d4
+            }
+            k <- k + 1
+        }
+    }
+    return(nabla)
+}
--- R/tcensReg_sepvar.R
+++ R/tcensReg_sepvar.R
@@ -19,37 +19,62 @@
 #' @return Returns a list of final estimate of theta, total number of iterations performed, initial log-likelihood,
 #' final log-likelihood, and estimated variance covariance matrix.
 
-tcensReg_sepvar <- function(formula, a = -Inf, v = NULL, group_var, theta_init = NULL, data = sys.frame(sys.parent()), ...){
-
+tcensReg_sepvar <- function(formula, a = -Inf, v = NULL, group_var,
+                            method=c("BFGS", "maxLik", "CG"),
+                            theta_init = NULL, data = sys.frame(sys.parent()),
+                            max_iter = 100,
+                            ...){
+  method <- match.arg(method)
   #checks for proper specification of formula
   if(class(formula)!= "formula"){
     stop("`formula` must be a formula", call. = FALSE)
   } else if (length(formula) != 3){
     stop("`formula` must be 2-sided", call. = FALSE)
   }
-  if(is.null(v) & a == -Inf){
-    warning("`v` and `a` are not specified indicating no censoring and no truncation", call. = FALSE)
-  }else if(is.null(v) & a!= -Inf){
-    warning("`v`is not specified indicating no censoring", call. = FALSE)
-  }else if(!is.null(v) & a == -Inf){
-    warning("`a` is not specified indicating no truncation", call. = FALSE)
-  }
+
+  y <- model.frame(formula, data)[, 1]
+  group <- data[, group_var]
+  if(!is.factor(group)) group <- as.factor(as.character(group))
+  X <- model.matrix(formula, data)
+
 
   #checking for proper specification of a and v
   if(!is.null(v) & (!is.numeric(a) | !is.numeric(v))){
     stop("`a` and `v` must both be numeric", call. = FALSE)
-  } else if(!is.null(v)& (length(a)!=1 | length(v)!=1)){
+  } else if(!is.null(v) & (length(a) !=1 | length(v) != 1)){
     stop("`a`, and `v` must both be scalars", call. = FALSE)
   }
+  # checking model specification of truncation and censoring params
+  if(is.null(v) & a == -Inf){
+    warning("`v` and `a` are not specified indicating no censoring and no truncation", call. = FALSE)
+  } else if(is.null(v) & a != -Inf){
+    if(any(y < a)){
+      stop("observed values below specified truncation `a`", call.=FALSE)
+    }
+    warning("`v`is not specified indicating no censoring", call. = FALSE)
+  } else if(!is.null(v) & a == -Inf){
+    len_cens <- sum(y == v, na.rm=TRUE)
+    if(len_cens == 0){
+      stop("censoring indicated but no observed censored values",
+           call.=FALSE)
+    }
+    warning("`a` is not specified indicating no truncation", call. = FALSE)
+  } else {
+    if(v < a) stop("censoring specified below truncation", call.=FALSE)
+    if(any(y < a)){
+      stop("observed values below specified truncation `a`", call.=FALSE)
+    }
+    len_cens <- sum(y == v, na.rm = TRUE)
+    if(len_cens == 0){
+      stop("censoring indicated but no observed censored values",
+           call.=FALSE)
+    }
+  }
+
 
   if(!group_var%in%names(model.frame(data))){
     stop("`group_var` must be a variable in the data.frame", call. = FALSE)
   }
-
-  y <- model.frame(formula, data)[, 1]
-  group <- data[, group_var]
-  X <- model.matrix(formula, data)
-  #here we have at least some explanatory variables
 
   #want to use different inital estimates depending on whether it is truncation only, censor only, or truncated and censored
   #if censored only, normal, or truncated only then use estimates from OLS
@@ -67,7 +92,43 @@ tcensReg_sepvar <- function(formula, a = -Inf, v = NULL, group_var, theta_init =
 
   names(theta_init)[1:(length(theta_init)-length(unique(group)))] <- colnames(X)
   names(theta_init)[(length(theta_init)-length(unique(group))+1):length(theta_init)] <- paste0("log_sigma", 1:length(unique(group)))
+
   #reading in the newton raphson for the truncated censored normal
-  results <- maxLik::maxLik(tcensReg_llike_sepvar_maxLik, start = theta_init)
+  if(method %in% c("BFGS", "CG")){
+    null_ll <- tcensReg_llike_sepvar(theta_init, y, X, group, a, v)
+
+    optim_results <- optim(par=theta_init,
+                           fn=tcensReg_llike_sepvar, gr=tcensReg_gradient_sepvar,
+                           method=method, hessian=TRUE,
+                           y=y, X=X, a=a, v=v, group=group,
+                           control=list(maxit = max_iter, fnscale=-1))
+    if(optim_results$convergence==1){
+        warning("Convergence unsuccessful: maximum iterations reached", call.=FALSE)
+      }
+    v_cov <- -solve(optim_results$hessian) #converting the hessian to estimate of variance
+    theta <- matrix(optim_results$par, nrow=length(optim_results$par), ncol=1)
+
+    # row.names(theta) <- c(colnames(X),"log_sigma") #adding in variable names
+    colnames(theta) <- "Estimate"
+    # row.names(v_cov) <- row.names(theta) #using the names for the variance covariance matrix
+    # colnames(v_cov) <- row.names(theta)
+    results <- list(theta = theta, convergence = optim_results$convergence,
+                initial_ll = null_ll, final_ll = optim_results$value, var_cov = v_cov,
+                method=method)
+  }
+  else if(method == "maxLik"){
+    left_trunc <- a
+    null_ll <- tcensReg_llike_sepvar_maxLik(theta_init, y, X, group, a, v)
+
+    maxLik_results <- maxLik::maxLik(logLik=tcensReg_llike_sepvar_maxLik,
+                                     grad=tcensReg_gradient_sepvar_maxLik,
+                                     start=theta_init,
+                                     y=y, X=X, left_trunc=left_trunc, v=v, group=group)
+    results <- list(theta = coef(maxLik_results), convergence = maxLik_results$code,
+                    initial_ll = null_ll, final_ll=summary(maxLik_results)$loglik,
+                    var_cov = vcov(maxLik_results), method = "maxLik")
+
+  }
+
   return(results)
 }
