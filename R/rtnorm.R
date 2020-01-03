b5c017e95263d7bd7bf2cbd4352ac4f0adca73f6
Justin Williams <williazo@ucla.edu>
2020-01-03 11:58:07 -0800

    correcting checks on the random truncated normal function. mu can be vector and sd can be scalar

--- R/rtnorm.R
+++ R/rtnorm.R
@@ -4,11 +4,12 @@
 #' first drawing from a random uniform distribution to generate c.d.f. probabilities and then the inverse density function is applied to generate observations.
 #'
 #' @param n Numeric scaler representing the number of observations. Must be greater than or equal to 1.
-#' @param mu Numeric vector of means
-#' @param sd Numeric vector of standard deviations.
+#' @param mu Mean value of the underlying normal random variable
+#' @param sd Standard deviation of underlying normal random variable
 #' @param a Numeric vector indicating the left-truncation value.
 #'
-#' @details Note that if the mean \code{mu} is specified as a vector then the standard deviation \code{sigma} must have the same length.
+#' @details Note that if the mean \code{mu} is specified as a vector then the standard deviation
+#' \code{sigma} must have the same length or be a scalar indicating that all samples have constant standard deviation.
 #'
 #' @examples
 #' #zero truncated normal data with mean 0.5 and standard deviation 1
@@ -16,20 +17,24 @@
 #'
 #' @importFrom stats runif qnorm pnorm
 #'
-#' @return Returns a vector of samples drawn from the specified distribition.
+#' @return Returns a vector of samples drawn from the specified distribition eqaul to length n.
 #'
 #' @export
 rtnorm <- function(n, mu, sd, a){
     probs <- stats::runif(n, 0, 1)
-    if(length(mu)>1){
-        if(sum(length(mu)==n, length(sd)==n) != 2) stop("Mean and standard deviation should be length 1 or length equal to n", call.=FALSE)
+    if(length(mu)==1 & length(sd) > 1) stop("Mean is scalar and standard deviation must also be scalar", call.=FALSE)
+    if(length(mu) > 1 ){
+        if(max(sum(length(mu)==n, length(sd)==n), sum(length(mu)==n, length(sd)==1)) != 2){
+            stop("Mean and standard deviation should be length 1 or length equal to n", call.=FALSE)
+        }
     } else{
         mu <- rep(mu, n)
         sd <- rep(sd, n)
     }
-    if(length(a)>1) stop("Truncation is assumed to be constant across population therefore a must be length 1", call.=FALSE)
+    if(length(a) > 1) stop("Truncation is assumed to be constant across population therefore a must be length 1", call.=FALSE)
     a <- rep(a, n)
     a_stand <- (a - mu) / sd
+    #inverse truncated normal cdf
     result <- qnorm((probs * (1 - pnorm(a_stand))) + pnorm(a_stand)) * sd + mu
     return(result)
 }
