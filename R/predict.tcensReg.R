5daf2f876dadc8be2e9c3c0b2f09417e0ac96027
Justin Williams <williazo@ucla.edu>
2020-03-27 15:05:05 -0700

    match S3 generic prediction method

--- R/predict.tcensReg.R
+++ R/predict.tcensReg.R
@@ -1,18 +1,18 @@
 #'  Method for Obtaining Fitted Predictions for tcensReg S3 Objects
 #'
-#' @param obj Object of class \code{tcensReg}
+#' @method predict tcensReg
+#'
+#' @param object Object of class \code{tcensReg}
 #' @param newdata New values to obtain predictions
 #'
-#' @return numeric vector of fitted values, i.e., yhat
+#' @importFrom stats predict
 #'
+#' @return numeric vector of fitted values, i.e., yhat
 #' @export
 #' @keywords internal
-
-
-
-predict.tcensReg <- function(obj, newdata=NULL){
-    design_mat <- obj$model_matrix
-    fixed_coef <- obj$theta[-grep("sigma", row.names(obj$theta))]
+predict.tcensReg <- function(object, newdata=NULL, ...){
+    design_mat <- object$model_matrix
+    fixed_coef <- object$theta[-grep("sigma", row.names(object$theta))]
     if(is.null(newdata)){
         y_hat <- design_mat %*% fixed_coef
     } else {
