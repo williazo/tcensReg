6095b677560c6cfe3f8504d9b5430592897b4664
Justin Williams <williazo@ucla.edu>
2020-03-27 15:09:18 -0700

    add additional details such as call, model_matrix, n_count, and latent_assumptiom. update details for the distributions estimable with this function

--- R/tcensReg.R
+++ R/tcensReg.R
@@ -1,22 +1,136 @@
-#' Regression Method for Truncated Normal Distribution with Censored Data
+#'  Regression Method for Truncated Normal Distribution with Censored Data
 #'
-#'  This function is used to find estimates from a linear equation assuming that the underlying distribution is truncated normal
-#'  and the data has subsequently been censored data. It uses analytically derived values of the gradient vector and Hessian matrix to
-#'  iteratively solve for the maximum likelihood using Newton-Raphson methods with step halving line search. This function can also
-#'  be used with censored only (similar to \code{\link[censReg]{censReg}}), truncated only (similar to \code{\link[truncreg]{truncreg}}), or uncensored and untruncated gaussian models.
+#'  This function is used to find estimates from a linear equation
+#'  assuming that the underlying distribution is truncated normal
+#'  and the data has subsequently been censored data.
 #'
 #' @param formula Object of class \code{formula} which symbolically describes the model to be fit
 #' @param a Numeric scalar indicating the truncation value. Initial value is -Inf indicating no truncation
 #' @param v Numeric scalar indicating the censoring value. Initially set to NULL indicating no censoring
 #' @param data Data.frame that contains the outcome and corresponding covariates. If none is provided then assumes objects are in user's environment.
-#' @param method Character value indicating which optimization routine to perform. Choices include \code{Newton}, \code{BFGS}, and \code{CG}. See details for explanation on each method.
-#' @param ... Additional arguments from \code{\link{tcensReg_newton}} such as \code{max_iter}, \code{step_max}, or \code{epsilon}.
+#' @param method Character value indicating which optimization routine to perform.
+#' Choices include \code{Newton}, \code{BFGS}, and \code{CG}. See details for explanation on each method.
+#' @param ... Additional arguments from such as \code{max_iter}, \code{step_max}, or \code{epsilon}.
+#' See details for how to define these additional arguments.
 #'
 #' @details
-#'  Currently available optimization routines include conjugate gradient (\code{CG}), Newton-Raphson (\code{Newton}), and BFGS (\code{BFGS}).
-#'  The default method is set as the conjugate gradient. Both the of the conjugate gradient and BFGS methods are implemented via the
-#'  general-purpose optimization \code{\link{optim}}. These two methods use only the respective likelihood and gradient functions.
-#'  The Newton-Raphson method uses the likelihood, gradient, and Hessian functions along with line search to achieve the maximum likelihood.
+#'  This estimation procedure returns maximum likelihood estimates under the presence
+#'  of \emph{left} truncation and/or \emph{left} censoring. It builds upon currently available methods
+#'  for limited dependent variables by relaxing the assumption of a latent normal distribution
+#'  and instead allows the this underlying distribution to potentially be a
+#'  latent \strong{truncated} normal distribution.
+#'
+#'  To indicate left censoring the user should specify the parameter \code{v}, and
+#'  to indicate left truncation specify the parameter \code{a}. If specifying
+#'  both left censoring and left truncation note that there is an implicit restriction that
+#'  \eqn{a<\nu}.
+#'
+#'  Below is a brief description of the types of distributions that can be fit
+#'  along with the assumed data generating process for the observed outcome, \eqn{Y}.
+#'
+#'  \subsection{Latent Distributions}{
+#'
+#'  The tcensReg function allows user to specify one of four combinations of
+#'  distributional assumptions with or without censoring. These are listed below
+#'  along with the necessary arguments needed to fit this model.
+#'    \subsection{Truncated Normal with Censoring}{
+#'
+#'    This is the main model that this package is designed to fit and introduced
+#'    in \insertCite{williams2019modeling}{tcensReg}.It assumes
+#'    \deqn{Y_{i}^{*}\sim TN(\mu, \sigma^{2}, a)}
+#'    where TN indicates a left truncated normal random variable, truncated at
+#'    the value \eqn{a}.
+#'
+#'    This underlying truncated normal random variable is then \emph{left} censored at the value
+#'    \eqn{\nu} to create the censored observations \eqn{Y} such that
+#'    \deqn{Y_{i}=\nu 1_{\{Y_{i}^{*}\le\nu\}}
+#'    + Y_{i}^{*}
+#'    1_{\{Y_{i}^{*}>\nu}\}}
+#'
+#'    Required Arguments:
+#'    \itemize{
+#'      \item \code{a}: left truncation value
+#'      \item \code{v}: left censoring value
+#'      }
+#'    }
+#'    \subsection{Normal with Censoring}{
+#'
+#'    This model is commonly referred to as the Tobit model
+#'    \insertCite{tobin1958estimation}{tcensReg}. This model assumes that the
+#'    data is generated from a latent normal random variable \eqn{Y_{i}^{*}}, i.e.,
+#'    \deqn{Y_{i}^{*}\sim N(\mu, \sigma^{2})}
+#'
+#'    This underlying normal random variable is then \emph{left} censored at the value
+#'    \eqn{\nu} to create the censored observations \eqn{Y} such that
+#'    \deqn{Y_{i}=\nu 1_{\{Y_{i}^{*}\le\nu\}}
+#'    + Y_{i}^{*}
+#'    1_{\{Y_{i}^{*}>\nu}\}}
+#'
+#'    Required Arguments:
+#'    \itemize{
+#'     \item \code{v}: left censoring value
+#'    }
+#'
+#'    This procedure can also be fit using the \code{\link[censReg]{censReg}} package by
+#'    \insertCite{henningsen2010estimating;textual}{tcensReg}.
+#'    }
+#'    \subsection{Truncated Normal}{
+#'
+#'      This model assumes that there is no censored observations, but that the
+#'      data are left truncated as originally described by
+#'      \insertCite{hald1949maximum;textual}{tcensReg}.
+#'
+#'      Therefore, we assume that the observed values follow
+#'      \deqn{Y_{i}^\sim TN(\mu, \sigma^{2}, a)}
+#'      where TN indicates a left truncated normal random variable, truncated at
+#'      the value \eqn{a}.
+#'
+#'      Required Arguments:
+#'      \itemize{
+#'       \item \code{a}: left truncation value
+#'      }
+#'
+#'      This procedure can also be fit using the \code{\link[truncreg]{truncreg}} package by
+#'      \insertCite{croissant2018truncreg;textual}{tcensReg}.
+#'     }
+#'     \subsection{Normal}{
+#'
+#'     This model assumes that there is no left censoring and no left truncation.
+#'
+#'     Maximum likelihood estimates are returned based on the assumption that the
+#'     random variable follows the distribution
+#'     \deqn{Y_{i}^\sim N(\mu, \sigma^{2})}
+#'
+#'     Required Arguments: None
+#'
+#'     This procedure can also be fit using the command \code{\link{lm}} in base R.
+#'     }
+#'  }
+#'
+#'  \subsection{Optimization Methods}{
+#'
+#'  Currently available optimization routines include conjugate gradient
+#'  (\code{CG}), Newton-Raphson (\code{Newton}), and BFGS (\code{BFGS}).
+#'  The default method is set as the conjugate gradient. Both the of the
+#'  conjugate gradient and BFGS methods are implemented via the
+#'  general-purpose optimization routine \code{\link{optim}}. These two methods
+#'  use only the respective likelihood and gradient functions.
+#'  The Newton-Raphson method uses the likelihood, gradient, and Hessian
+#'  functions along with line search to achieve the maximum likelihood.
+#'  }
+#'
+#'  \subsection{Additional Arguments}{
+#'
+#'  There are additional arguments that the user may provide for controlling the
+#'  optimization routine.
+#'  \itemize{
+#'    \item{max_iter: Maximum number of iterations for optimization routine. Default is 100}
+#'    \item{step_max: Maximum number of steps when performing line search. Default is 10}
+#'    \item{epsilon: Numeric value used to define algorithm stops, i.e., when evaluated gradient is less than epsilon. Default is 0.001}
+#'    \item{tol_val: Tolerance value used to stop the algorithm if the (n+1) and (n) log likelihood is within the tolerance limit.}
+#'    \item{theta_init: Numeric vector specifying the initial values to use for the estimated parameters \eqn{\beta} and \eqn{\log(\sigma)}}
+#'  }
+#'  }
 #'
 #' @importFrom stats model.frame model.matrix
 #'
@@ -28,14 +142,17 @@
 #' y <- ifelse(y_star <= 0.25, 0.25, y_star)
 #'
 #' #find MLE estimates
-#' tcensReg(y ~ 1, v = 0.25, a = 0)
+#' trunc_cens_mod <- tcensReg(y ~ 1, v = 0.25, a = 0)
+#' summary(trunc_cens_mod)
+#'
 #'
 #' @return Returns a list of final estimate of theta, total number of iterations performed, initial log-likelihood,
-#' final log-likelihood, estimated variance covariance matrix, information criterion, and model design matrix.
+#' final log-likelihood, estimated variance covariance matrix, information criterion, model design matrix,
+#' call, list of total observations and censored observations, and latent distributional assumption.
 #'
 #' @export
-
-
+#' @references
+#'     \insertAllCited{}
 tcensReg <- function(formula, a = -Inf, v = NULL, data = sys.frame(sys.parent()),
                      method = c("CG", "Newton", "BFGS"), ...){
     #checks for proper specification of formula
@@ -50,6 +167,10 @@ tcensReg <- function(formula, a = -Inf, v = NULL, data = sys.frame(sys.parent())
     y <- model.frame(formula, data)[, 1]
     #design matrix
     X <- model.matrix(formula, data)
+
+    #observed censoring
+    n_total <- length(y)
+    n_cens <- sum(y==v)
 
     #checking for proper specification of a and v
     if(!is.null(v) & (!is.numeric(a) | !is.numeric(v))){
@@ -96,9 +217,17 @@ tcensReg <- function(formula, a = -Inf, v = NULL, data = sys.frame(sys.parent())
     bic <- log(length(y)) * length(results$theta) - 2 * results$final_ll
     info_criteria <- c(AIC=aic, BIC=bic)
     results[["info_criteria"]] <- info_criteria
+
+    #additional information to save in order to use S3 methods
     results$model_matrix <- X
-
+    results$call <- match.call()
+    results$n_count <- list(n=n_total, n_cens=n_cens)
+    latent_assumption <- ifelse(is.null(v) & a == -Inf, "Normal",
+                                ifelse(is.null(v) & a != -Inf, "Truncated Normal",
+                                       ifelse(!is.null(v) & a == -Inf, "Normal with Censoring",
+                                              ifelse(!is.null(v) & a != -Inf, "Truncated Normal with Censoring"))))
+    results$latent_assumption <- latent_assumption
+    #saving object as S3
     class(results) <- "tcensReg"
-
     return(results)
 }
