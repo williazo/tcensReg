944e44c03373d4eb762a225c53f7b054102f3883
Justin Williams <williazo@ucla.edu>
2020-03-27 15:03:42 -0700

    renaming function parameters to match S3 generic

--- R/coef.tcensReg.R
+++ R/coef.tcensReg.R
@@ -1,17 +1,18 @@
 #'  Method for Extracting Coefficient Estimates for tcensReg S3 Objects
 #'
-#' @param obj Object of class \code{tcensReg}
+#' @method coef tcensReg
+#'
+#' @param object Object of class \code{tcensReg}
 #' @param logsigma Logical indicator of whether to return standard deviation as logsigma or sigma. Default is FALSE returning sigma
 #' @param digits Numeric scalar indicating the number of digits to return
+#' @param ... Extra arguments
 #'
 #' @return numeric named vector of coefficient estimates
 #' @export
 #' @keywords internal
-
-
-coef.tcensReg <- function(obj, logsigma=FALSE, digits=4){
-    theta_result <- c(obj$theta)
-    names(theta_result) <- row.names(obj$theta)
+coef.tcensReg <- function(object, logsigma=FALSE, digits=4, ...){
+    theta_result <- c(object$theta)
+    names(theta_result) <- row.names(object$theta)
     if(logsigma==FALSE){
         sigma_ind <- grep("log_sigma", names(theta_result))
         theta_result[sigma_ind] <- exp(theta_result[sigma_ind])
