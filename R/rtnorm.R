81a7fd5a2757b5a1afd96aea0426c7e771eb10ee
Justin Williams <williazo@ucla.edu>
2020-12-21 15:15:51 -0800

    update random truncated normal function to include right truncation as option

--- R/rtnorm.R
+++ R/rtnorm.R
@@ -1,25 +1,32 @@
-#' @title Simulate Random Left-Truncated Normal Distribution
+#' @title Simulate Random Left-Truncated And/Or Right-Truncated Normal
+#' Distribution
 #'
-#' @description This function is used to generate random samples from left-truncated normal distribution with specified mean and variance.
+#' @description This function is used to generate random samples from
+#' left-truncated and or right-truncated normal distribution with specified mean
+#' and variance.
 #'
-#' @param n Numeric scalar representing the number of observations. Must be greater than or equal to 1.
-#' @param mu Mean value of the underlying normal random variable
-#' @param sd Standard deviation of underlying normal random variable
-#' @param a Numeric vector indicating the left-truncation value.
+#' @param n Numeric scalar representing the number of observations. Must be
+#' greater than or equal to 1.
+#' @param mu Mean value of the underlying normal random variable. Default is 0
+#' @param sd Standard deviation of underlying normal random variable. Default is
+#' 1
+#' @param a Numeric vector indicating the left-truncation value. Default is -Inf
+#' @param b Numeric vector indicating the right-truncation value. Default is Inf
 #'
 #' @details
 #' Our goal is to draw samples from the left truncated normal random variable
 #' \eqn{Y_{i}^{*}}. We define this distribution as
-#' \deqn{Y_{i}^{*}\sim TN(\mu, \sigma^{2}, a)}
+#' \deqn{Y_{i}^{*}\sim TN(\mu, \sigma^{2}, a, b)}
 #'
 #' Sampling is performed by first drawing from a random variable \eqn{Z} with a
 #' uniform distribution on the interval \eqn{[0, 1]} to
-#' generate cumulative density probabilities, \eqn{p}. Then the inverse density function
+#' generate cumulative density probabilities, \eqn{p}. Then the inverse density
+#' function
 #' of the truncated normal random variable is applied to generate our desired
 #' truncated normal observations.
 #'
 #' This inverse truncated normal function is shown below:
-#' \deqn{Y_{i}^{*}=\Phi^{-1}\Bigg\{p\times\bigg[1-\Phi\big(\frac{a-\mu}{\sigma}\big)\bigg]
+#' \deqn{Y_{i}^{*}=\Phi^{-1}\Bigg\{p\times\bigg[\Phi\big(\frac{b-\mu}{\sigma}\big)-\Phi\big(\frac{a-\mu}{\sigma}\big)\bigg]
 #' + \Phi\big(\frac{a-\mu}{\sigma}\big)\Bigg\}\times\sigma+\mu,}
 #' where \eqn{p} represents the probabilities sampled from the uniform
 #' distribution.
@@ -36,8 +43,8 @@
 #' }
 #' }
 #' @examples
-#' #zero truncated normal data with mean 0.5 and standard deviation 1
-#' y_star <- rtnorm(n = 100, mu = 0.5, sd = 1, a = 0)
+#' #zero left-truncated normal data with mean 0.5 and standard deviation 1
+#' y_star <- rtnorm(n = 100, mu = 0.5, sd = 1, a = 0, b = Inf)
 #'
 #' @importFrom stats runif qnorm pnorm
 #'
@@ -45,21 +52,60 @@
 #' distribution equal to length n.
 #'
 #' @export
-rtnorm <- function(n, mu, sd, a){
+rtnorm <- function(n,
+                   mu = 0,
+                   sd = 1,
+                   a = -Inf,
+                   b = Inf) {
+
     probs <- stats::runif(n, 0, 1)
-    if(length(mu)==1 & length(sd) > 1) stop("Mean is scalar and standard deviation must also be scalar", call.=FALSE)
-    if(length(mu) > 1 ){
-        if(max(sum(length(mu)==n, length(sd)==n), sum(length(mu)==n, length(sd)==1)) != 2){
-            stop("Mean and standard deviation should be length 1 or length equal to n", call.=FALSE)
+    if (length(mu) == 1 & length(sd) > 1) {
+        stop(
+            "Mean is scalar and standard deviation must also be scalar",
+            call. = FALSE
+            )
+    }
+    if (length(mu) > 1) {
+        if (
+            max(
+                sum(length(mu) == n, length(sd) == n),
+                sum(length(mu) == n, length(sd) == 1
+                    )
+                ) != 2
+            ) {
+            stop(
+                "Mean and standard deviation should be length 1 or equal to n",
+                call. = FALSE
+                )
         }
     } else{
         mu <- rep(mu, n)
         sd <- rep(sd, n)
     }
-    if(length(a) > 1) stop("Truncation is assumed to be constant across population therefore a must be length 1", call.=FALSE)
+    if (any(length(a) > 1, length(b) > 1)) {
+        stop("Truncation values must be length 1", call. = FALSE)
+    }
+    if (a >= b) {
+        stop(
+            "Left-truncation value greater than or equal to right-truncation",
+             call. = FALSE
+             )
+    }
+    if (b == Inf & a == -Inf) {
+        message("No truncation specified")
+    } else if (b != Inf & a == -Inf) {
+        message("Right truncation only")
+    } else if (b == Inf & a != -Inf) {
+        message("Left truncation only")
+    } else if (b != Inf & a != -Inf) {
+        message("Left and right truncation")
+    }
     a <- rep(a, n)
+    b <- rep(b, n)
     a_stand <- (a - mu) / sd
+    b_stand <- (b - mu) / sd
     #inverse truncated normal cdf
-    result <- qnorm((probs * (1 - pnorm(a_stand))) + pnorm(a_stand)) * sd + mu
+    trunc_adj <- pnorm(b_stand) - pnorm(a_stand)
+    result <- qnorm((probs * (trunc_adj)) + pnorm(a_stand)) * sd + mu
     return(result)
 }
